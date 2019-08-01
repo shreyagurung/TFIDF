f8764aacb485487532f7459dd75c2e588cf07e0f
Shreya Gurung <23639687+shreyagurung@users.noreply.github.com>
2019-08-01 19:40:48 +0530

    Extract words from cheese.txt to a python list

--- cheese.py
+++ cheese.py
@@ -11,16 +11,5 @@ class cheese:
 		with open (file1, 'r') as myfile:
 			data=myfile.read().replace("\n","")
 		list_cheese.append(data)
-	#for item in list_cheese:
-	#	print(item)
 	
 		cheeseStr=str(list_cheese)
-
-  	#for item in list_cheese:
-	#	for plural in item.split():
-	#		list_cheese2.append(stemmer.stem(plural))
-	#	cheeseStr=' '.join(list_cheese2)
-	#print(cheeseStr)
-	#for item in list_cheese2:
-	#	print(item)
-	#cheeseStr=str(list_cheese2)
